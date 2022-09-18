a610250c04a1fb26e03eb8b76feba8e6d8bbd3d0
AvaKhA <109901314+AvaKhA@users.noreply.github.com>
2022-09-18 16:48:30 +0430

    added the relevant icon from the weather API
    added:
    - relevant icon
    - wind speed
    -humidity
    -description

--- index.js
+++ index.js
@@ -43,18 +43,72 @@ searchForm.addEventListener("keydown", function (event) {
 function generateWeatherUrl(city) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCity);
-  axios.get(url).then(showTemperature);
+  axios.get(url).then(showInformation);
 }
 
-//show city and temperature
-function showCity(response) {
+//show city and temperature and date
+function showInformation(response) {
   let cityName = document.querySelector("#city-name");
+  let temperatureElement = document.querySelector("#current-temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateTimeElement = document.querySelector("#date-time");
+  let iconElement = document.querySelector("#current-icon");
+
   cityName.innerHTML = `${response.data.name}`;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} Km/h`;
+  dateTimeElement.innerHTML = showDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  console.log(response.data);
 }
 
-function showTemperature(response) {
-  let currentTemp = Math.round(response.data.main.temp);
-  let currentTempBox = document.querySelector("#current-temperature");
-  currentTempBox.innerHTML = `current temperature: ${currentTemp}°C`;
+function showDate(timestamp) {
+  let now = new Date(timestamp);
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
+
+// celsius and fahrenheit
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
